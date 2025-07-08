488bc5039b7bdb70478a1eb4d807f15246c0ace5
Mukti Raj Bhusal <muktirajbhusal.cgc@gmail.com>
2025-07-09 00:09:07 +0530

    Migration basic, Making post, Mass Assignment Vulnerabilities, Connecting two database using belongto and hasmany relations

--- LaracastLaravel/blog/routes/web.php
+++ LaracastLaravel/blog/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\Post;
+use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 use Spatie\YamlFrontMatter\YamlFrontMatter;
 
@@ -20,13 +21,13 @@ Route::get('/', function (){
     return view("posts", ["posts" => $posts]);
 });
 
-Route::get("posts/{post}", function($id){
+Route::get("posts/{post:slug}", function(Post $post){
 
     // find a post by its slug and pass it to a view called post
 
 
 
-    return view("post", ['post' => Post::findOrFail($id)]);
+    return view("post", ['post' => $post]);
 
     // if(!file_exists($path =  __DIR__ . "/../resources/posts/{$slug}.html" )){
     //     // dd("File not found: {$path}");
@@ -37,4 +38,9 @@ Route::get("posts/{post}", function($id){
 
     // // $post = file_get_contents($path);
     // return view("post",["post"=> $post]);
+});
+
+Route::get('categories/{category:slug}', function(Category $category){
+    $posts = Post::all();
+    return view("posts", ["posts" => $category->posts]);
 });
